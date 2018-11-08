c105542e9b1d6b46f0251bfcd2d44e4dd7620980
s15264 <44864109+s15264@users.noreply.github.com>
2018-11-08 12:44:52 +0100

    Update 4f.js

--- 4f.js
+++ 4f.js
@@ -1,24 +1,24 @@
-				function isPrime(x){
-					if (x===1){
-					return false;
-					}
-					if (x===2){
-					return true;
-					}
-					else{
-					for(var i = 2; i < x; i++){
-						if(x % i == 0){ 
-						return false;
-						}	
-						if (x !== 1 && x !== 0){
-						return true;
-						}
-					}
-					}
+	function isPrime(x){
+		if (x===1){
+		return false;
+		}
+		if (x===2){
+		return true;
+		}
+		else{
+		for(var i = 2; i < x; i++){
+			if(x % i == 0){ 
+			return false;
+			}	
+			if (x !== 1 && x !== 0){
+			return true;
 			}
-			console.log(isPrime(1));
-			console.log(isPrime(2));
-			console.log(isPrime(3));
-			console.log(isPrime(4));
-			console.log(isPrime(5));
-			console.log(isPrime(6));
+		}
+		}
+}
+console.log(isPrime(1));
+console.log(isPrime(2));
+console.log(isPrime(3));
+console.log(isPrime(4));
+console.log(isPrime(5));
+console.log(isPrime(6));
