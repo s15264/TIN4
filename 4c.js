bcd7aeaacfdc82506b7da9bae81c6fc79411d886
s15264 <44864109+s15264@users.noreply.github.com>
2018-11-08 12:47:51 +0100

    Update 4c.js

--- 4c.js
+++ 4c.js
@@ -4,9 +4,9 @@ var removeChar = p.replace(/[^A-Z0-9]/ig, "").toLowerCase();
 var checkPalindrome = removeChar.split('').reverse().join('');
 
 if(removeChar === checkPalindrome){
-  document.write("<div>"+ p + "a Palindrome <div>");
+  return true;
 }else{
-  document.write("<div>" + p + "not a Palindrome </div>");
+  return false;
 }
 }
 palindrome('"kajak"')
