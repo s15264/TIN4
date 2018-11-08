892442da4a3aa8609ab32e9cb062c2fc9e90cc61
s15264 <44864109+s15264@users.noreply.github.com>
2018-11-08 12:28:48 +0100

    Add files via upload

---
+++ 4a.js
@@ -0,0 +1,13 @@
+
+			function Factorial(x){
+				  var a = 1;
+				  for(i=1;i<=x;i++){
+					a *= i;	
+				  }
+				console.log(a);
+				
+			}
+
+			Factorial(2);
+			Factorial(3);
+			Factorial(5);
