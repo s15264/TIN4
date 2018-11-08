920f1321fb561b625b51e6bc69cd66548e4f08e5
s15264 <44864109+s15264@users.noreply.github.com>
2018-11-08 12:45:26 +0100

    Update 4d.js

--- 4d.js
+++ 4d.js
@@ -1,6 +1,6 @@
-			function Alphabetical(str){
-			var x = str.split("");
-			return x.sort().join("");
-			}
-			
-			console.log(Alphabetical("webmaster"));
+function Alphabetical(str){
+var x = str.split("");
+return x.sort().join("");
+}
+
+console.log(Alphabetical("webmaster"));
